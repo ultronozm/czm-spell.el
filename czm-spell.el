8b162ccc4e2c6421254d177cd35b1f658241f35c
Paul Nelson <ultrono@gmail.com>
2024-02-07 05:10:36 +0100

    re-add unversioned auctex dependency
    This seems to be the way?

--- czm-spell.el
+++ czm-spell.el
@@ -5,7 +5,7 @@
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
 ;; Version: 0.0
 ;; URL: https://github.com/ultronozm/czm-spell.el
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (auctex))
 ;; Keywords: tex, tools, abbrev, convenience
 
 ;; This program is free software; you can redistribute it and/or modify

330e88a3a469b0f4dce0858f32695a56efa4bb70
Paul Nelson <ultrono@gmail.com>
2026-06-17 16:45:45 +0200

    Simplify spell-check by removing abbrev integration
    * czm-spell.el: Update summary line and keywords.
    (czm-spell-then-abbrev): Remove abbrev-saving bindings
    (ispell-save-corrections-to-abbrev, ispell-use-global-abbrev-table).
    Simplify docstring and control flow.
    (czm-spell-setup): Remove function with hardcoded
    ispell-tex-skip-alists, now handled by upstream Emacs.

--- czm-spell.el
+++ czm-spell.el
@@ -1,4 +1,4 @@
-;;; czm-spell.el --- Spell-check that saves corrections in abbrevs  -*- lexical-binding: t; -*-
+;;; czm-spell.el --- Spell-check helper  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023  Paul D. Nelson
 
@@ -6,7 +6,7 @@
 ;; Version: 0.0
 ;; URL: https://github.com/ultronozm/czm-spell.el
 ;; Package-Requires: ((emacs "25.1") (auctex))
-;; Keywords: tex, tools, abbrev, convenience
+;; Keywords: tex, tools, convenience
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -25,11 +25,10 @@
 
 ;; This package provides a no-frills spell-check function
 ;; `czm-spell-then-abbrev' that searches backwards in the visible
-;; buffer for a misspelled word, offers a list of corrections, and
-;; uses the native `ispell-save-corrections-to-abbrev' option to store
-;; the chosen correction in the abbrev table.  It is designed to work
-;; well with TeX buffers: it will not offer corrections for words
-;; inside math environments, citations, labels or commands.
+;; buffer for a misspelled word and offers a list of corrections.  It
+;; is designed to work well with TeX buffers: it will not offer
+;; corrections for words inside math environments, citations, labels
+;; or commands.
 ;; 
 ;; Some of the code here was adapted from
 ;; https://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html,
@@ -43,7 +42,7 @@
 ;;   :bind ("s-;" . czm-spell-then-abbrev))
 ;;
 ;;
-;;  TODO: compare with (flyspell-abbrev-p t)?  Maybe something?
+;;  TODO: compare with flyspell?  Maybe something?
 
 ;;; Code:
 
@@ -52,57 +51,54 @@
 
 ;;;###autoload
 (defun czm-spell-then-abbrev (p)
-  "Call `ispell-word', then create an abbrev for it.
-With prefix P, create local abbrev.  Otherwise it will be global.
+  "Spell-check word at/before point.
+
+Prefix argument P is accepted for backward compatibility but currently ignored.
 If there's nothing wrong with the word at point, keep looking for
 a typo until the beginning of the visible window.  You can skip
 typos you don't want to fix with `SPC', and you can abort
 completely with `C-g'."
   (interactive "P")
-  (let ((ispell-save-corrections-to-abbrev t)
-        (ispell-use-global-abbrev-table (not p))
-        (start (window-start))
+  (ignore p)
+  (let ((start (window-start))
         corrected)
     (save-excursion
       (backward-word)
-      (while
-          (let ((bef (car-safe
-                      (save-excursion
-                        (ignore-errors
-                          (ispell-get-word t))))))
-            (if (and
-                 (>= (point) start)
-                 bef
-                 (looking-at bef)
-                 (not (or
-                       (let ((face (plist-get (text-properties-at (point))
-                                              'face)))
-                         (or
-                          (memq face '(tex-math font-latex-math-face))
-                          (and (listp face)
-                               (or (memq 'tex-math face)
-                                   (memq 'font-latex-math-face face)))))
-                       (and
-                        (or (eq major-mode 'latex-mode)
-                            (eq major-mode 'LaTeX-mode))
-                        (or (TeX-in-comment)
-                            (czm-spell--inside-ref-label-or-cite-p)))
-                       (looking-back
-                        (regexp-opt
-                         (list
-                          "\\" "[" "{" "}"))
-                        (1- (point))))))
-                ;; Word was corrected or user quit.
-                (let ((result (ispell-word t 'quiet)))
-                  (cond
-                   ((eq result 'quit) nil)
-                   (result (setq corrected t)
-                           nil)
-                   (t
-                    ;; Also end if we reach `bob'.
-                    (not (bobp)))))
-              ;; If there's no word at point, keep looking until `bob'.
-              (not (bobp))))
+      (while (if (and
+                  (>= (point) start)
+                  (let ((bef (car-safe
+                              (save-excursion
+                                (ignore-errors
+                                  (ispell-get-word t))))))
+                    (and
+                     bef
+                     (looking-at bef)
+                     (not (or
+                           (let ((face (plist-get (text-properties-at (point))
+                                                  'face)))
+                             (or
+                              (memq face '(tex-math font-latex-math-face))
+                              (and (listp face)
+                                   (or (memq 'tex-math face)
+                                       (memq 'font-latex-math-face face)))))
+                           (and
+                            (or (eq major-mode 'latex-mode)
+                                (eq major-mode 'LaTeX-mode))
+                            (or (TeX-in-comment)
+                                (czm-spell--inside-ref-label-or-cite-p)))
+                           (looking-back
+                            (regexp-opt
+                             (list
+                              "\\" "[" "{" "}"))
+                            (1- (point))))))))
+                 ;; Word was corrected or user quit.
+                 (let ((result (ispell-word t 'quiet)))
+                   (cond
+                    ((eq result 'quit) nil)
+                    (result (setq corrected t) nil)
+                    (t (not (bobp)))))
+               ;; If there's no word at point, keep looking until `bob'.
+               (not (bobp)))
         (backward-word)))
     (unless corrected
       (user-error "No typo at or before point"))))
@@ -128,48 +124,6 @@ completely with `C-g'."
 		              nil
 	             t))
 	       nil))))
-
-
-
-(defun czm-spell-setup ()
-  "Setup czm-spell."
-  (setq ispell-tex-skip-alists
-	'((("\\\\\\(hyperref\\|specialrule\\)" ispell-tex-arg-end 3)
-	   ("\\\\\\(C\\(?:pagerefrange\\|refrange\\*?\\)\\|LTXtable\\|SI\\|c\\(?:pagerefrange\\|refrange\\*?\\)\\|hyperdef\\|inputminted\\|mdfapptodefinestyle\\|v\\(?:pagerefrange\\*?\\|refrange\\*?\\)\\)" ispell-tex-arg-end 2)
-	   ("\\\\\\(Ac\\(?:f\\(?:p\\*\\|[*p]\\)\\|l\\(?:p\\*\\|[*p]\\)\\|p\\*\\|single\\*?\\|[*flp]\\)?\\|C\\(?:pageref\\|ref\\*?\\)\\|DeleteShortVerb\\|Fref\\|Iac\\|MakeShortVerb\\*?\\|Nameref\\|Ref\\|V\\(?:erbatimInput\\|ref\\)\\|a\\(?:c\\(?:a\\(?:p\\*\\|[*p]\\)\\|f\\(?:like\\*?\\|p\\(?:\\*\\|like\\*?\\)\\|[*p]\\)\\|l\\(?:p\\*\\|[*p]\\)\\|p\\*\\|reset\\|s\\(?:etup\\|ingle\\*?\\|p\\*\\|[*p]\\)\\|use\\|[*aflps]\\)?\\|ddfontfeatures\\|ttachfile\\(?:setup\\)?\\|uto\\(?:pageref\\*?\\|ref\\*?\\)\\)\\|c\\(?:aption\\(?:of\\*?\\|setup\\*?\\)\\|learcaptionsetup\\*?\\|pageref\\|ref\\*?\\|s\\)\\|empheqset\\|f\\(?:igureversion\\|ref\\|ullref\\|vset\\)\\|h\\(?:ref\\|yper\\(?:baseurl\\|image\\|link\\|setup\\|target\\)\\)\\|i\\(?:ac[ls]\\|nputpygments\\)\\|l\\(?:abelcref\\|cnamecref\\|st\\(?:inputlisting\\|set\\)\\)\\|mdfsetup\\|n\\(?:ame\\(?:[Cc]?ref\\)\\|olinkurl\\|um\\)\\|s\\(?:et\\(?:minted\\(?:inline\\)?\\|py\\(?:gments\\(?:fv\\|pygopt\\)\\|thontexfv\\)\\)\\|i\\(?:ndex\\|setup\\)?\\)\\|t\\(?:cbset\\(?:foreverylayer\\)?\\|extattachfile\\)\\|u\\(?:rl\\|se\\(?:\\(?:prin\\|stdou\\)tpythontex\\)\\)\\|v\\(?:pageref\\*?\\|ref\\*?\\)\\)" ispell-tex-arg-end)
-	   ("\\\\\\(addlinespace\\|captionlistentry\\|tcb\\(?:item\\|ox\\)\\)" ispell-tex-arg-end 0)
-	   ("\\\\\\(?:mint\\(?:inline\\)?\\|pygment\\)" TeX-ispell-tex-arg-verb-end 1)
-	   ("\\\\\\(?:Verb\\|lstinline\\|py\\(?:con[cv]?\\|lab\\(?:con[cv]?\\|[bcsv]\\)?\\|[bcsv]\\)?\\|sympy\\(?:con[cv]?\\|[bcsv]\\)?\\)" TeX-ispell-tex-arg-verb-end)
-	   ("\\\\fontspec" TeX-ispell-tex-arg-end 1 1 0)
-	   ("\\\\cmidrule" . "{[-0-9]+}")
-	   ("\\\\raisebox" TeX-ispell-tex-arg-end 1 2 0)
-	   ("\\\\(" . "\\\\)")
-	   ("\\\\addcontentsline" . #3=(ispell-tex-arg-end 2))
-	   ("\\\\add\\(tocontents\\|vspace\\)" . #1=(ispell-tex-arg-end))
-	   ("\\\\\\([aA]lph\\|arabic\\)" . #1#)
-	   ("\\\\cref" . #1#)
-	   ("\\\\eqref" . #1#)
-	   ("\\\\cite" . #1#)
-	   ("\\\\bibliographystyle" . #1#)
-	   ("\\\\makebox" . #2=(ispell-tex-arg-end 0))
-	   ("\\\\e?psfig" . #1#)
-	   ("\\\\document\\(class\\|style\\)" . "\\\\begin[ 	
-]*{document}"))
-	  (("\\(BVerbatim\\*?\\|LVerbatim\\*?\\|SaveVerbatim\\|Verbatim\\(?:\\*\\|Out\\)?\\|align\\(?:\\*\\|at\\*?\\)?\\|d\\(?:array\\*?\\|group\\*?\\|math\\*?\\|series\\*?\\)\\|empheq\\|flalign\\*?\\|gather\\*?\\|lstlisting\\|m\\(?:inted\\|ultline\\*?\\)\\|py\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|gments\\|lab\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|sub\\|verbatim\\)\\|sub\\|verbatim\\)\\|sympy\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|sub\\|verbatim\\)\\|tikzpicture\\)" . "\\\\end{\\(BVerbatim\\*?\\|LVerbatim\\*?\\|SaveVerbatim\\|Verbatim\\(?:\\*\\|Out\\)?\\|align\\(?:\\*\\|at\\*?\\)?\\|d\\(?:array\\*?\\|group\\*?\\|math\\*?\\|series\\*?\\)\\|empheq\\|flalign\\*?\\|gather\\*?\\|lstlisting\\|m\\(?:inted\\|ultline\\*?\\)\\|py\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|gments\\|lab\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|sub\\|verbatim\\)\\|sub\\|verbatim\\)\\|sympy\\(?:block\\|co\\(?:de\\|n\\(?:code\\|sole\\|verbatim\\)\\)\\|sub\\|verbatim\\)\\|tikzpicture\\)}")
-	   ("\\(description\\*?\\|enumerate\\*?\\|itemize\\*?\\|mdframed\\|tc\\(?:b\\(?:itemize\\|raster\\)\\|olorbox\\)\\)" ispell-tex-arg-end 0)
-	   ("xltabular" ispell-tex-arg-end 2)
-	   ("tcboxed\\(raster\\|itemize\\)" ispell-tex-arg-end)
-	   ("tabular[*xy]" TeX-ispell-tex-arg-end)
-	   ("stabular\\*" TeX-ispell-tex-arg-end)
-	   ("stabular" ispell-tex-arg-end)
-	   ("equation" ispell-tex-arg-end)
-	   ("equation\\*" ispell-tex-arg-end)
-	   ("filecontents\\*?" ispell-tex-arg-end)
-	   ("\\(figure\\|table\\)\\*?" . #2#)
-	   ("list" . #3#)
-	   ("program" . "\\\\end[ 	]*{program}")
-	   ("verbatim\\*?" . "\\\\end[ 	]*{verbatim\\*?}")))))
-
 
 (provide 'czm-spell)
 ;;; czm-spell.el ends here
